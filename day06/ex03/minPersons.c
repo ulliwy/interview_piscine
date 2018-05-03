ec7f0f2beb24b34ab9308ead5da255e390aa5973
ulliwy <inessa.t@icloud.com>
2018-05-03 10:16:01 -0700

    day06 ex03 done

--- day06/ex03/minPersons.c
+++ day06/ex03/minPersons.c
@@ -3,13 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   minPersons.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iprokofy <iprokofy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Ulliwy <Ulliwy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 15:19:10 by iprokofy          #+#    #+#             */
-/*   Updated: 2018/05/02 15:19:40 by iprokofy         ###   ########.fr       */
+/*   Updated: 2018/05/02 22:18:55 by Ulliwy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "header.h"
+#include <math.h>
 
-int minPersons(int elements, int minPercentage);
+int minPersons(int elements, int minPercentage)
+{
+	long double p;
+	int n = 2;
+	while (1)
+	{
+		p = 1;
+		for (int i = 1; i < n; i++)
+		{
+			p = p * (1 - (long double)i / (long double)elements);
+		}
+		p = 1 - p;
+		if (p * 100 > minPercentage)
+			return n;
+		else
+			n++;
+	}
+	return n;
+}
